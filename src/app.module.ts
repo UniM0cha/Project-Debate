836d3c2ac8ad98bf7469777f6ebd117d599b80a5
UniM0cha <solst_ice@naver.com>
2022-01-02 23:11:24 +0900

    0102 - 데이터베이스 테스트: 테이블 만들어짐, 하지만 객체형식에 대해서 좀 더 알아봐야할 듯 하다...

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { User } from './users/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    // mysql config
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'debate',
+      password: 'mochamocha',
+      database: 'debate',
+      // entities: [User],
+      // 이 옵션은 forFeature() 메소드를 통해 등록된 모든 엔티티가 자동 추가된다.
+      autoLoadEntities: true,
+
+      // Setting synchronize makes sure your entities will be synced with the database, every time you run the application.
+      // synchronize: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
