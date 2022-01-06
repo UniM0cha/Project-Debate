a7998a9a894414f707690beb477a328d0d7ee520
ksemin99 <70834817+ksemin99@users.noreply.github.com>
2022-01-06 23:04:16 +0900

    01/06 홈페이지 기능 구성중

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
+  let isLogined = window.sessionStorage.getItem('isLogined');
+  console.log(isLogined);
   // 상단 메뉴바 _ 이전 주제 리스트 버튼
 
   // 상단 메뉴바 _ 가운데 로고
   // 다시 돌아가는데 리스트 내용 실시간으로 바뀐거 받아와야 함
 
   //상단 메뉴바 _ 로그인 버튼
+  const loginform = document.querySelector('.form-login');
+  const logoutform = document.querySelector('.form-logout');
+
+  loginform.style.display = 'none';
+  logoutform.style.display = 'block';
 
   //토론 주제 시간
   const time = document.querySelector('.time');
@@ -22,51 +29,59 @@ document.addEventListener('DOMContentLoaded', function () {
   let agree = 50;
   let disagree = 50;
 
-  //로그인 안하고 그냥 볼 때 찬성 반대 비율 바 보여주기
-  //   agree_btn.style.display = 'none';
-  //   disagree_btn.style.display = 'none';
-  //   agree_bar.style.display = 'block';
-  //   disagree_bar.style.display = 'block';
+  if (isLogined == false) {
+    //로그인 안했을 시 그냥 볼 때 찬성 반대 비율 바 보여주기
+    agree_btn.style.display = 'none';
+    disagree_btn.style.display = 'none';
+    agree_bar.style.display = 'block';
+    disagree_bar.style.display = 'block';
 
-  //   agree_bar.style.width = agree + '%';
-  //   disagree_bar.style.width = disagree + '%';
+    agree_bar.style.width = agree + '%';
+    disagree_bar.style.width = disagree + '%';
+  }
 
   //로그인 했을때
-  //찬성 반대 바 숨기기
-  agree_bar.style.display = 'none';
-  disagree_bar.style.display = 'none';
+  if (isLogined == true) {
+    //찬성 반대 바 숨기기
+    agree_bar.style.display = 'none';
+    disagree_bar.style.display = 'none';
 
-  agree_btn.addEventListener('click', () => {
-    agree = agree + 1;
-    disagree = disagree - 1;
-    console.log(agree);
-    console.log(disagree);
-    //찬성 반대 선택 이후 버튼 숨기기
-    agree_btn.style.display = 'none';
-    disagree_btn.style.display = 'none';
-    //찬성 반대 비율 변경
-    agree_bar.style.width = agree + '%';
-    disagree_bar.style.width = disagree + '%';
-    //찬성 반대 비율 바 보여주기
-    agree_bar.style.display = 'block';
-    disagree_bar.style.display = 'block';
-  });
+    agree_btn.addEventListener('click', () => {
+      agree = agree + 1;
+      disagree = disagree - 1;
+      console.log(agree);
+      console.log(disagree);
+      //찬성 반대 선택 이후 버튼 숨기기
+      agree_btn.style.display = 'none';
+      disagree_btn.style.display = 'none';
+      //찬성 반대 비율 변경
+      agree_bar.style.width = agree + '%';
+      disagree_bar.style.width = disagree + '%';
+      //찬성 반대 비율 바 보여주기
+      agree_bar.style.display = 'block';
+      disagree_bar.style.display = 'block';
 
-  disagree_btn.addEventListener('click', () => {
-    agree = agree - 1;
-    disagree = disagree + 1;
-    console.log(agree);
-    console.log(disagree);
-    //찬성 반대 선택 이후 버튼 숨기기
-    agree_btn.style.display = 'none';
-    disagree_btn.style.display = 'none';
-    //찬성 반대 비율 변경
-    agree_bar.style.width = agree + '%';
-    disagree_bar.style.width = disagree + '%';
-    //찬성 반대 비율 바 보여주기
-    agree_bar.style.display = 'block';
-    disagree_bar.style.display = 'block';
-  });
+      //찬성 시 찬성을 세션에 저장해줘야함
+    });
+
+    disagree_btn.addEventListener('click', () => {
+      agree = agree - 1;
+      disagree = disagree + 1;
+      console.log(agree);
+      console.log(disagree);
+      //찬성 반대 선택 이후 버튼 숨기기
+      agree_btn.style.display = 'none';
+      disagree_btn.style.display = 'none';
+      //찬성 반대 비율 변경
+      agree_bar.style.width = agree + '%';
+      disagree_bar.style.width = disagree + '%';
+      //찬성 반대 비율 바 보여주기
+      agree_bar.style.display = 'block';
+      disagree_bar.style.display = 'block';
+
+      //반대 시 반대를 세션에 저장해줘야함
+    });
+  }
 
   //채팅 입력 부분
   const ul = document.querySelector('.live-debate');
@@ -74,11 +89,13 @@ document.addEventListener('DOMContentLoaded', function () {
   const send = document.querySelector('.send');
 
   send.addEventListener('click', () => {
-    add_agree_opinion('Hi', opinion_input, 'time');
+    add_agree_opinion('Hi', opinion_input.value, 'time');
+    displaycontainer.scrollTo(0, displaycontainer.scrollHeight);
   });
 
   function add_agree_opinion(nickname, opinion, time) {
     const li = document.createElement('li');
+    li.classList.add('agree');
     //클래스 agree와 disagree추가
     const dom = `
         <div class='nickname'>${nickname}</div>
