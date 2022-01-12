098e08de0e1ff84c8dbd1c893cab79d7603e10e2
ksemin99 <70834817+ksemin99@users.noreply.github.com>
2022-01-13 00:23:23 +0900

    0113 indexjs구현중

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,67 @@
 let socket = io.connect('http://localhost:3000/message');
 
 document.addEventListener('DOMContentLoaded', function () {
-  //채팅 입력 부분
+
+
+
+  //찬성반대 비율바 구현
+  const agree_bar = document.querySelector('.agree-bar');
+  const disagree_bar = document.querySelector('.disagree-bar');
+  const agree_btn = document.querySelector('.agree-btn');
+  const disagree_btn = document.querySelector('.disagree-btn');
+
+  let sum
+  let agree
+  let disagree
+  let agreebar_width
+  let disagreebar_width
+
+  function calculate() {
+    
+  }
+
+  if(isLogined == true){
+      agree_bar.style.display = 'none';
+      disagree_bar.style.display = 'none';
+
+      agree_btn.addEventListener('click', () => {
+        agree = agree + 1;
+        disagree = disagree - 1;
+        //찬성 반대 선택 이후 버튼 숨기기
+        agree_btn.style.display = 'none';
+        disagree_btn.style.display = 'none';
+        //찬성 반대 비율 변경
+        agree_bar.style.width = agree + '%';
+        disagree_bar.style.width = disagree + '%';
+        //찬성 반대 비율 바 보여주기
+        agree_bar.style.display = 'block';
+        disagree_bar.style.display = 'block';
+      });
+
+      disagree_btn.addEventListener('click', () => {
+        agree = agree - 1;
+        disagree = disagree + 1;
+        console.log(agree);
+        console.log(disagree);
+        //찬성 반대 선택 이후 버튼 숨기기
+        agree_btn.style.display = 'none';
+        disagree_btn.style.display = 'none';
+        //찬성 반대 비율 변경
+        agree_bar.style.width = agree + '%';
+        disagree_bar.style.width = disagree + '%';
+        //찬성 반대 비율 바 보여주기
+        agree_bar.style.display = 'block';
+        disagree_bar.style.display = 'block';
+      });
+
+  }else{
+    agree_btn.style.display = 'none';
+    disagree_btn.style.display = 'none';
+  }
+  //찬성반대 비율바 구현
+    
+    
+  //실시간 채팅 구현
   const ul = document.querySelector('.live-debate');
   const send = document.querySelector('#send');
 
@@ -9,7 +69,9 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log(data);
 
     //let opinion = opinion_input;
-    send_opinion('Hi', data.newmessage, 'time');
+    send_opinion(nickname, data.newmessage, 'time');
+    console.log(isLogined);
+    console.log(nickname);
   });
 
   send.addEventListener('click', (e) => {
@@ -42,4 +104,5 @@ document.addEventListener('DOMContentLoaded', function () {
     ul.appendChild(li);
     ul.scrollTop = ul.scrollHeight;
   }
+  //실시간 채팅 구현
 });
