6021e7b9661291831e45f9cc64a501c4aa3f3a6a
ksemin99 <70834817+ksemin99@users.noreply.github.com>
2022-01-13 20:58:25 +0900

    0113 practice

---
+++ public/js/ex_debate_list.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const topic1 = document.querySelector('topic1');
+    topic1.addEventListener('click', () => {
+        location.href="index.hbs"
+    })
+});
