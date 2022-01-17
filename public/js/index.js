eb2b8fd5b1b5fe35cad9d8953d6b3cf025656717
ksemin99 <70834817+ksemin99@users.noreply.github.com>
2022-01-17 21:14:38 +0900

    0117 showRemaining추가

--- public/js/index.js
+++ public/js/index.js
@@ -58,6 +58,7 @@ const countDownTimer = function (id, date) {
     document.getElementById(id).textContent += minutes + '분 ';
     document.getElementById(id).textContent += seconds + '초';
   }
+  showRemaining();
   timer = setInterval(showRemaining, 1000);
 };
 
