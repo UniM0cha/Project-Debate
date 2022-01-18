f6dbe23d9b9968ec92a20d2fb51256c90be7cda6
ksemin99 <70834817+ksemin99@users.noreply.github.com>
2022-01-18 16:53:28 +0900

    0118 오류 수정

--- public/js/index.js
+++ public/js/index.js
@@ -43,7 +43,7 @@ const countDownTimer = function (id, date) {
 
       //
       return;
-    } else if ((isLogined = false && distDt > 0)) {
+    } else if ((isLogined === false && distDt > 0)) {
       input_box.style.display = 'block';
       send_btn.style.display = 'block';
     }
