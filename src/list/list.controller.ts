0302062b98bbfe0262b97f108ceb7720da402e51
qsc0415 <qsc0415@naver.com>
2022-02-01 23:21:43 +0900

    0201 - 이전 리스트 끝나는 시간 불러오기 삽질....
    데이터베이스 추가할거임

--- src/list/list.controller.ts
+++ src/list/list.controller.ts
@@ -26,40 +26,108 @@ export class ListController {
   ) {}
   private readonly logger = new Logger(ListController.name);
 
-  @Get('/')
-  async getAllTopics(@Res() res, @Session() session) {
+  @Get('/page/:page')
+  async getAllTopics(
+    @Res() res,
+    @Session() session,
+    @Param('page', ParseIntPipe) page: number,
+  ) {
     // await this.topicServices.addTestData();
     // const topicReserves: TopicReserve[] =
     //   await this.topicServices.findAllTopicReservesWithTopic();
     const topics: Topic[] = await this.topicServices.findAllTopics();
-    const getPassedTopicList: TopicReserve[] =
+    let getPassedTopicList: TopicReserve[] =
       await this.topicServices.findPASSEDTopicReservesWithTopic();
-    const topicReserveCount: Number = await this.topicServices.getPassedCount();
-    const topicReservePageCount: Number = Math.floor(
-      ((await this.topicServices.getPassedCount()) - 1) / 10 + 1,
+    // const getPassedTopicList: TopicReserve[] =
+    //   await this.topicServices.findPASSEDTopicReservesWithTopic();
+    const topicReserveCount: number = await this.topicServices.getPassedCount();
+    const topicReservePageCount: number = Math.floor(
+      (topicReserveCount - 1) / 10 + 1,
     );
-    console.log('최대값 : ' + topicReserveCount);
-    console.log('페이지 수 : ' + topicReservePageCount);
 
     const getEndTime: TopicReserve[] = await this.topicServices.getPassedList();
-    const getEndTimeList: TopicReserve[] = getEndTime.map((getEndTime) => {
+    const getEndTimeList: Date[] = getEndTime.map((getEndTime) => {
       getEndTime.reserveDate.setDate(getEndTime.reserveDate.getDate() - 1);
-      return getEndTime;
+      return getEndTime.reserveDate;
     });
+
+    // const getEndTimeList: Date[] = getEndTime.map((getEndTime) => {
+    //   getEndTime.reserveDate.setDate(getEndTime.reserveDate.getDate() - 1);
+    //   return getEndTime.reserveDate;
+    // });
     getEndTimeList.reverse();
-    console.log(getPassedTopicList);
+
+    // getPassedTopicList = getPassedTopicList.slice((page - 1) * 10, page * 10);
+
     let arr1 = [];
-    for( let i: number = 1; i <= topicReservePageCount; i++ ) {
-      if(topicReserveCount.valueOf() / i >= 10) {
-        arr1.push( getPassedTopicList.slice( (i - 1) * 10, i * 10 ) );
-      } else {
-        arr1.push( getPassedTopicList.slice( (i - 1) * 10,  topicReserveCount.valueOf() ) );
-        console.log( getPassedTopicList.slice( (i - 1) * 10,  topicReserveCount.valueOf() ) );
-      }
-    }
-    console.log("arr1 : " + arr1);        // test
-    console.log("arr1 : " + arr1.length); // test
-    
+
+    getPassedTopicList.forEach((element, index) => {
+      arr1.push({
+        reserveId: element.reserveId,
+        reserveDate: element.reserveDate,
+        reserveEndDate: getEndTimeList[index + 1],
+        topic: {
+          topicName: element.topic.topicName,
+        },
+      });
+    });
+    this.logger.debug(`arr1: ${JSON.stringify(arr1, null, 4)}`);
+    arr1 = arr1.slice((page - 1) * 10, page * 10);
+
+    /*
+    this.logger.debug(
+      `getPassedTopicList: ${JSON.stringify(getPassedTopicList, null, 4)}`,
+    );
+    const arr1 = {
+      getPassedTopicList: getPassedTopicList,
+      getEndTimeList: getEndTimeList,
+    };
+    this.logger.debug(`arr1: ${JSON.stringify(arr1, null, 4)}`);
+
+    const arr2 = arr1.map((arr1) => {
+      return {
+        reserveId: arr1.getPassedTopicList.reserveId,
+        reserveDate: arr1.getPassedTopicList.reserveDate,
+        reserveEndDate: arr1.getEndTimeList,
+        topic: {
+          topicName: arr1.getPassedTopicList.topic.topicName,
+        },
+      };
+    });
+    this.logger.debug(`arr2: ${JSON.stringify(arr2, null, 4)}`);
+    */
+
+    // const arr2 = arr1.map((arr) => {
+    //   return {
+    //     reserveId: arr.getPassedTopicList.reserveId
+    //     reserveDate: arr.reserveDate,
+
+    //   };
+    // });
+
+    // arr1.push(getPassedTopicList.slice((page - 1) * 10, page * 10));
+    // getPassedTopicList = getPassedTopicList.slice((page - 1) * 10, page * 10);
+
+    //console.log('arr1 : ' + JSON.stringify(getPassedTopicList, null, 4)); // test
+    // console.log('arr1 : ' + JSON.stringify(arr1, null, 4)); // test
+
+    // [[1,2,3,4,5,6,7,8,9,10],[11]]
+
+    /**
+     * /list/page/1
+     *
+     * 데이터가 1 ~ 25 있다고 쳐봐
+     *
+     * 1 -> 1 ~ 10
+     * 2 -> 11 ~ 20
+     * 3 -> 21 ~ 25
+     *
+     *
+     * 2 -> 11 ~ 20
+     * x -> (10x-9) ~ 10x
+     *
+     *
+     */
 
     //return res.render('ex_debate_list', { topics: topics });
 
@@ -68,8 +136,8 @@ export class ListController {
     return res.render('ex_debate_list', {
       arr1: arr1, // test
       ...viewDto,
-      getPassedTopicList: getPassedTopicList,
-      getEndTimeList: getEndTimeList,
+      //getPassedTopicList: getPassedTopicList,
+      //getEndTimeList: getEndTimeList,
       //topicReserves: topicReserves,
       topics: topics,
       topicReserveCount: topicReserveCount,
