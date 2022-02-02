73e6c58d14d845dc860e08c2615e6c9f51eadc98
ksemin99 <70834817+ksemin99@users.noreply.github.com>
2022-02-02 21:24:15 +0900

    02/02 자잘한 버그 수정중

--- public/js/index.js
+++ public/js/index.js
@@ -341,9 +341,11 @@ async function send_chat_socket_emit() {
   document.querySelector('#message').value = '';
 }
 
+const ul = document.querySelector('.live-debate');
+
 // 서버로부터 도착한 채팅을 표시하는 함수
 function send_opinion(nickname, opinion, date, opinionType) {
-  const ul = document.querySelector('.live-debate');
+  
   const today = new Date(date);
   let hour = today.getHours();
   let min = today.getMinutes();
@@ -400,6 +402,59 @@ async function getAllChat() {
   } else if (data.state === 0) {
     ///////////////////////////////////// 모든 채팅 내용은 chatList 안에 저장됩니다 ///////////////////
     const chatList = data.chat;
+    //채팅 돌리기
+    chatList.forEach(item => {
+      //시간 계산
+      const today = new Date(item.chatDate);
+      let hour = today.getHours();
+      let min = today.getMinutes();
+      let ampm = '';
+      if (hour > 12) {
+        hour = hour - 12;
+        ampm = '오후';
+      } else {
+        ampm = '오전';
+      }
+      hour = hour.toString().padStart(2, '0');
+      min = min.toString().padStart(2, '0');
+      //시간 계산
+
+      const nickname = item.nickname;
+      const opinion = item.chatMessage;
+      const time = ampm + ' ' + hour + ':' + min
+      const opinionType = item.opinionType
+      const ul = document.querySelector('.live-debate');
+
+      //채팅 작성하기
+      ul.scrollTop = ul.scrollHeight;
+      if (opinionType === 'agree') {
+        const li = document.createElement('li');
+        li.classList.add(opinionType);
+        //클래스 agree와 disagree추가
+        const dom = `
+            <div class='nickname'>${nickname}</div>
+              <div
+                class='user-opinion'
+              >${opinion}</div>
+              <div class='opinion-time'>${time}</div>
+            `;
+        li.innerHTML = dom;
+        ul.appendChild(li);
+      } else if (opinionType === 'disagree') {
+        const li = document.createElement('li');
+        li.classList.add(opinionType);
+        //클래스 agree와 disagree추가
+        const dom = `
+            <div class='nickname'>${nickname}</div>
+            <div class='opinion-time'>${time}</div>
+              <div
+                class='user-opinion'
+              >${opinion}</div>
+            `;
+        li.innerHTML = dom;
+        ul.appendChild(li);
+      }
+    });
     console.log(`chatList: ${JSON.stringify(chatList, null, 4)}`);
   }
   return;
