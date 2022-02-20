f2aea6a709176c18ad6e013616b0586befcacc18
UniM0cha <solst_ice@naver.com>
2022-02-20 23:17:03 +0900

    0220 - 오류페이지 필터 추가 / profile 페이지 대충 만듦 / 프로필 수정 api 만들어야함

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -5,33 +5,34 @@ import {
   Render,
   Req,
   Session,
+  UseFilters,
   UseGuards,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from 'src/auth/auth.service';
 import { AuthDto } from 'src/auth/dto/auth.dto';
+import { HttpExceptionFilter } from 'src/http-exception.filter';
+import { UsersService } from 'src/users/users.service';
 
 @Controller('profile')
+@UseFilters(new HttpExceptionFilter())
 export class ProfileController {
   private readonly logger = new Logger(ProfileController.name);
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly usersService: UsersService,
+  ) {}
 
   @Get('/')
   @UseGuards(AuthGuard('jwt'))
   @Render('profile')
   async showProfile(@Req() req, @Session() session) {
-    // 세션에서 로그인이 되어있는지 확인 - 안되어있으면 접근할 수 없도록
-    // if (
-    //   !(
-    //     (session.userData && session.userData.userId)
-    //     // (await this.authService.validateUser(session.userData.userId))
-    //   )
-    // ) {
-    //   throw new UnauthorizedException();
-    // }
-    // const authDto: AuthDto = await this.authService.setAuthDto(session);
-    // this.logger.debug(`viewDto: ${JSON.stringify(authDto)}`);
-    // return authDto;
+    const authDto: AuthDto = await this.authService.setAuthDto(
+      req?.user?.userId,
+    );
+    this.logger.debug(`authDto: ${JSON.stringify(authDto)}`);
+    return authDto;
+
     // 이메일 - 변경 불가
     // 별명 - 중복 확인 코드 삽입
     // 적용 / 취소 버튼
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -117,6 +117,20 @@ export class UsersService {
     return user ? user.nickname : null;
   }
 
+  async getUserData(
+    userId: string,
+  ): Promise<{ nickname: string; profileImage: string; email: string }> {
+    const user: Users = await this.usersRepository.findOne({
+      select: ['nickname', 'profileImage', 'email'],
+      where: { userId: userId },
+    });
+    return {
+      nickname: user?.nickname,
+      profileImage: user?.profileImage,
+      email: user?.email,
+    };
+  }
+
   async findUserRole(userId: string): Promise<UserRole> {
     const user: Users = await this.usersRepository.findOne({
       select: ['role'],
